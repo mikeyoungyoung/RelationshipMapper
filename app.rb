e066833ad0ef30c0512f0522bbcb3e692bace5d4
mikeyoungyoung <mikeyoung.young@gmail.com>
2013-11-26 10:05:54 -0500

    updated build

--- app.rb
+++ app.rb
@@ -7,11 +7,22 @@ require 'bundler/setup'
 require_relative 'classes'
 #include FileUtils::Verbose
 
-rel_1 = Relationship.new(:partner,6)
-puts rel_1.strength
-owns_1 = Owns.new(:strength => 8)
-owns_2 = Owns.new
-puts owns_1.strength
-puts owns_1.type
-puts owns_2.strength
-puts owns_2.type
+John = Business_Partner.new(:name => "John")
+BUT000 << [John.name, John.type]
+Paul = Business_Partner.new(:name => "Paul")
+BUT000 << [Paul.name, Paul.type]
+BUT000.each {|x| puts "#{x}"}
+
+
+#rel_1 = Relationship.new(:type => :partner, :strength => 10)
+#puts rel_1.strength
+owns_1 = Owns.new(:partner_1 => John, :partner_2 => Paul)
+
+partners = Array.new()
+
+#create set of partners
+partners = [*1..10]
+
+#relationships
+Relationships[1] = [owns_1.partner_1.name, owns_1.type, "#{owns_1.partner_2.name}, with strength", owns_1.strength]
+Relationships.each { |a,b,c,d,e| puts "#{a} #{b} #{c} #{d} #{e}"}
--- classes.rb
+++ classes.rb
@@ -1,8 +1,13 @@
+#Global Hashes
+BUT000 = Array.new(2)
+
 class Relationship
-    attr_accessor :type, :strength
-    def initialize(type, strength)
-        @type = :type
-        @strength = strength
+    attr_accessor :type, :strength, :partner_1, :partner_2
+    def initialize(params = {})
+        @type = params.fetch(:type, 'Relationship')
+        @strength = params.fetch(:strength, 1)
+        @partner_1 = params.fetch(:partner_1)
+        @partner_2 = params.fetch(:partner_2)
     end
 end
 
@@ -10,13 +15,17 @@ class Owns < Relationship
     def initialize(params = {})
         @strength = params.fetch(:strength, 1)
         @type = :owns
+        @partner_1 = params.fetch(:partner_1)
+        @partner_2 = params.fetch(:partner_2)
     end
 end
 
 class Business_Partner
     attr_accessor :name, :type
-    def initialize(name,type)
-        @name = :name
-        @type = :type
+    def initialize(params = {})
+        @name = params.fetch(:name, '########')
+        @type = params.fetch(:type, 'Business Partner')
     end
 end
+
+Relationships = Array.new(5)
